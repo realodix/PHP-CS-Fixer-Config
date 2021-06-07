acdd39b6b6552dc5596a2eac7a24f2f7309b9842
Kei <budi03@gmail.com>
2021-06-07 20:54:39 +0700

    Update Joomla.php

--- src/RuleSet/Joomla.php
+++ src/RuleSet/Joomla.php
@@ -16,6 +16,7 @@ final class Joomla extends AbstractRuleSet implements RuleSetExplicitInterface
      * - [M] class_attributes_separation - New option syntax
      *
      * - [M] lowercase_constants - Rename to constant_case
+     * - [M] no_extra_consecutive_blank_lines - Rename to no_extra_blank_lines
      */
     public function myRules(): array
     {
@@ -37,25 +38,25 @@ final class Joomla extends AbstractRuleSet implements RuleSetExplicitInterface
             'no_trailing_whitespace'        => true,
             'visibility_required'           => true,
             // symfony
-            'no_whitespace_before_comma_in_array'   => true,
-            'whitespace_after_comma_in_array'       => true,
-            'no_empty_statement'                    => true,
-            'simplified_null_return'                => true,
-            'no_extra_consecutive_blank_lines'      => true,
+            'blank_line_before_return'              => true,
+            'cast_spaces'                           => true,
             'function_typehint_space'               => true,
             'include'                               => true,
             'no_alias_functions'                    => true,
-            'no_trailing_comma_in_list_call'        => true,
-            'trailing_comma_in_multiline_array'     => true,
             'no_blank_lines_after_class_opening'    => true,
-            'phpdoc_trim'                           => true,
-            'blank_line_before_return'              => true,
+            'no_empty_statement'                    => true,
+            'no_extra_blank_lines'                  => true,
+            'no_trailing_comma_in_list_call'        => true,
             'no_trailing_comma_in_singleline_array' => true,
-            'single_blank_line_before_namespace'    => true,
-            'cast_spaces'                           => true,
             'no_unneeded_control_parentheses'       => true,
             'no_unused_imports'                     => true,
+            'no_whitespace_before_comma_in_array'   => true,
             'no_whitespace_in_blank_line'           => true,
+            'phpdoc_trim'                           => true,
+            'simplified_null_return'                => true,
+            'single_blank_line_before_namespace'    => true,
+            'trailing_comma_in_multiline_array'     => true,
+            'whitespace_after_comma_in_array'       => true,
             // contrib
             'concat_space' => ['spacing' => 'one'],
         ];
