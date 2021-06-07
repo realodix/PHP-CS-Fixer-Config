66a40fd4bf176f226a329861798ee879a52ef436
Kei <budi03@gmail.com>
2021-06-07 16:08:27 +0700

    Update Spatie.php

--- src/RuleSet/Spatie.php
+++ src/RuleSet/Spatie.php
@@ -12,31 +12,29 @@ final class Spatie extends AbstractRuleSet implements RuleSetExplicitInterface
      * See https://github.com/spatie/laravel-permission/blob/master/.php_cs.dist.php
      *
      * Diff
-     * - [R] array_syntax - Default ['syntax' => 'short']
+     * - [R] array_syntax - same as the default value
+     * - [R] ordered_imports - same as the default value
+     * - [R] blank_line_before_statement - same as the default value
+     * - [R] method_argument_space.on_multiline - same as the default value
      */
     public function myRules(): array
     {
         $rules = [
             '@PSR2'                             => true,
-            'ordered_imports'                   => ['sort_algorithm' => 'alpha'],
             'no_unused_imports'                 => true,
             'not_operator_with_successor_space' => true,
             'trailing_comma_in_multiline'       => true,
             'phpdoc_scalar'                     => true,
             'unary_operator_spaces'             => true,
             'binary_operator_spaces'            => true,
-            'blank_line_before_statement'       => [
-                'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
-            ],
-            'phpdoc_single_line_var_spacing' => true,
-            'phpdoc_var_without_name'        => true,
-            'class_attributes_separation'    => [
+            'phpdoc_single_line_var_spacing'    => true,
+            'phpdoc_var_without_name'           => true,
+            'class_attributes_separation'       => [
                 'elements' => [
                     'method' => 'one',
                 ],
             ],
             'method_argument_space' => [
-                'on_multiline'                     => 'ensure_fully_multiline',
                 'keep_multiple_spaces_after_comma' => true,
             ],
             'single_trait_insert_per_statement' => true,
