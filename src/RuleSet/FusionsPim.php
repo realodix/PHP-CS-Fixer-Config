b43618c260d0a2028022974e2a8fb7eb25630222
Kei <budi03@gmail.com>
2021-06-08 01:03:49 +0700

    Update FusionsPim.php

--- src/RuleSet/FusionsPim.php
+++ src/RuleSet/FusionsPim.php
@@ -3,18 +3,46 @@
 namespace Realodix\PhpCsFixerConfig\RuleSet;
 
 use FusionsPim\PhpCsFixer\Factory;
+use PhpCsFixerCustomFixers\Fixer;
 
 final class FusionsPim extends AbstractRuleSet implements RuleSetExplicitInterface
 {
     protected $name = 'Fusions PIM';
 
     /**
-     * Based on symfony/symfony
+     * Based on fusionspim/php-cs-fixer-config
      *
-     * See https://github.com/symfony/symfony/blob/5.4/.php-cs-fixer.dist.php
+     * See https://github.com/fusionspim/php-cs-fixer-config/blob/master/src/Factory.php
+     *
+     * There are 1 rules found commented:
+     * - NoUselessParenthesisFixer::name()
      */
     public function myRules(): array
     {
-        return Factory::fromDefaults();
+        $rules = Factory::DEFAULT_RULES();
+        // The extraRules() method cannot be imported because it is a private method
+        $extraRules = [
+            Fixer\CommentedOutFunctionFixer::name()                 => ['print_r', 'var_dump', 'var_export'],
+            Fixer\CommentSurroundedBySpacesFixer::name()            => true,
+            Fixer\DataProviderNameFixer::name()                     => ['prefix' => '', 'suffix' => '_data_provider'],
+            Fixer\DataProviderReturnTypeFixer::name()               => true,
+            Fixer\InternalClassCasingFixer::name()                  => true,
+            Fixer\NoCommentedOutCodeFixer::name()                   => true,
+            Fixer\NoDoctrineMigrationsGeneratedCommentFixer::name() => true,
+            Fixer\NoDuplicatedArrayKeyFixer::name()                 => true,
+            Fixer\NoDuplicatedImportsFixer::name()                  => true,
+            Fixer\NoPhpStormGeneratedCommentFixer::name()           => true,
+            Fixer\NoSuperfluousConcatenationFixer::name()           => true,
+            Fixer\NoUselessCommentFixer::name()                     => true,
+            Fixer\NoUselessDoctrineRepositoryCommentFixer::name()   => true,
+            Fixer\NoUselessStrlenFixer::name()                      => true,
+            Fixer\PhpUnitNoUselessReturnFixer::name()               => true,
+            Fixer\PhpdocNoIncorrectVarAnnotationFixer::name()       => true,
+            Fixer\PhpdocSingleLineVarFixer::name()                  => true,
+            Fixer\SingleSpaceAfterStatementFixer::name()            => true,
+            Fixer\SingleSpaceBeforeStatementFixer::name()           => true,
+        ];
+
+        return array_merge($rules, $extraRules);
     }
 }
