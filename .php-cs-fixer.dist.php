3e65a8c808c98bbc959623cec07ed806604c2175
Kei <budi03@gmail.com>
2021-06-07 21:35:08 +0700

    tests ok

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,7 @@
 use Realodix\PhpCsFixerConfig\Factory;
 use Realodix\PhpCsFixerConfig\RuleSet;
 
-$config = Factory::fromRuleSet(new RuleSet\Typo3());
+$config = Factory::fromRuleSet(new RuleSet\Realodix());
 $config->getFinder()->in(__DIR__);
 
 return $config;
