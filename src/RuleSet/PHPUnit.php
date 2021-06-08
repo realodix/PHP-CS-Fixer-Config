a44b16d8a61d494515519e48a71d051af779ce36
Kei <budi03@gmail.com>
2021-06-08 12:50:48 +0700

    Update PHPUnit.php

--- src/RuleSet/PHPUnit.php
+++ src/RuleSet/PHPUnit.php
@@ -11,10 +11,17 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
      * https://github.com/sebastianbergmann/phpunit/blob/master/.php-cs-fixer.dist.php
      *
      * Diff
-     * - [D] array_syntax                           Same as the default value
-     * - [D] header_comment                         Must be set manually
-     * - [D] class_attributes_separation            Same as the default value
      * - [D] global_namespace_import.import_classes Same as the default value
+     * - [D] header_comment                         Must be set manually
+     * - [M] array_syntax                           Same as the default value
+     * - [M] class_attributes_separation            Same as the default value
+     * - [M] declare_equal_normalize                Same as the default value
+     * - [M] list_syntax                            Same as the default value
+     * - [M] method_argument_space                  Same as the default value
+     * - [M] ordered_interfaces                     Same as the default value
+     * - [M] return_type_declaration                Same as the default value
+     * - [M] trailing_comma_in_multiline            Same as the default value
+     * - [M] visibility_required                    Same as the default value
      */
     public function myRules(): array
     {
@@ -23,16 +30,10 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'array_indentation'       => true,
             'array_push'              => true,
             'backtick_to_shell_exec'  => true,
-            'binary_operator_spaces'  => [
-                'operators' => [
-                    '='  => 'align_single_space_minimal',
-                    '=>' => 'align_single_space_minimal',
-                    '|'  => null,
-                ],
-            ],
             'blank_line_after_namespace'  => true,
             'braces'                      => true,
             'cast_spaces'                 => true,
+            'class_attributes_separation'  => true,
             'class_definition'             => true,
             'clean_namespace'              => true,
             'combine_consecutive_issets'   => true,
@@ -41,7 +42,7 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'compact_nullable_typehint'    => true,
             'concat_space'                 => ['spacing' => 'one'],
             'constant_case'                => true,
-            'declare_equal_normalize'      => ['space' => 'none'],
+            'declare_equal_normalize' => true,
             'declare_strict_types'         => true,
             'dir_constant'                 => true,
             'echo_tag_syntax'              => true,
@@ -65,14 +66,14 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'is_null'                         => true,
             'lambda_not_used_import'          => true,
             'line_ending'                     => true,
-            'list_syntax'                     => ['syntax' => 'short'],
+            'list_syntax' => true,
             'logical_operators'               => true,
             'lowercase_cast'                  => true,
             'lowercase_keywords'              => true,
             'lowercase_static_reference'      => true,
             'magic_constant_casing'           => true,
             'magic_method_casing'             => true,
-            'method_argument_space'           => ['on_multiline' => 'ensure_fully_multiline'],
+            'method_argument_space' => true,
             'modernize_types_casting'         => true,
             'native_constant_invocation'      => false,
             'native_function_casing'          => false,
@@ -114,32 +115,8 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'non_printable_character'         => true,
             'normalize_index_brace'           => true,
             'operator_linebreak'              => ['only_booleans' => true, 'position' => 'end'],
-            'ordered_class_elements'          => [
-                'order' => [
-                    'use_trait',
-                    'constant_public',
-                    'constant_protected',
-                    'constant_private',
-                    'property_public_static',
-                    'property_protected_static',
-                    'property_private_static',
-                    'property_public',
-                    'property_protected',
-                    'property_private',
-                    'method_public_static',
-                    'construct',
-                    'destruct',
-                    'magic',
-                    'phpunit',
-                    'method_public',
-                    'method_protected',
-                    'method_private',
-                    'method_protected_static',
-                    'method_private_static',
-                ],
-            ],
             'ordered_imports' => ['imports_order' => ['const', 'class', 'function'],],
-            'ordered_interfaces'            => ['direction' => 'ascend', 'order' => 'alpha'],
+            'ordered_interfaces'            => true,
             'ordered_traits'                => true,
             'phpdoc_align'                  => true,
             'phpdoc_annotation_without_dot' => true,
@@ -151,14 +128,7 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'phpdoc_no_package'             => true,
             'phpdoc_no_useless_inheritdoc'  => true,
             'phpdoc_order'                  => true,
-            'phpdoc_order_by_value'         => [
-                'annotations' => [
-                    'covers',
-                    'dataProvider',
-                    'throws',
-                    'uses',
-                ],
-            ],
+            'phpdoc_order_by_value'         => ['annotations' => [ 'covers', 'dataProvider', 'throws', 'uses',],],
             'phpdoc_return_self_reference'   => true,
             'phpdoc_scalar'                  => true,
             'phpdoc_separation'              => true,
@@ -174,7 +144,7 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'pow_to_exponentiation'          => true,
             'protected_to_private'           => true,
             'return_assignment'              => true,
-            'return_type_declaration'        => ['space_before' => 'none'],
+            'return_type_declaration'        => true,
             'self_accessor'                  => true,
             'self_static_accessor'           => true,
             'semicolon_after_instruction'    => true,
@@ -198,16 +168,10 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'ternary_operator_spaces'        => true,
             'ternary_to_elvis_operator'      => true,
             'ternary_to_null_coalescing'     => true,
-            'trailing_comma_in_multiline'    => ['elements' => ['arrays']],
+            'trailing_comma_in_multiline'    => true,
             'trim_array_spaces'              => true,
             'unary_operator_spaces'          => true,
-            'visibility_required'            => [
-                'elements' => [
-                    'const',
-                    'method',
-                    'property',
-                ],
-            ],
+            'visibility_required'            => true,
             'void_return'                     => true,
             'whitespace_after_comma_in_array' => true,
 
@@ -235,11 +199,27 @@ final class PHPUnit extends AbstractRuleSet implements RuleSetExplicitInterface
             'no_multiline_whitespace_around_double_arrow'   => true,
             'no_singleline_whitespace_before_semicolons'    => true,
             'phpdoc_trim_consecutive_blank_line_separation' => true,
+            'binary_operator_spaces'  => [
+                'operators' => [
+                    '='  => 'align_single_space_minimal',
+                    '=>' => 'align_single_space_minimal',
+                    '|'  => null,
+                ],
+            ],
             'blank_line_before_statement' => [
                 'statements' => [
                     'break', 'continue', 'declare', 'default', 'do', 'exit', 'for',
                     'foreach', 'goto', 'if', 'include', 'include_once', 'require',
                     'require_once', 'return', 'switch', 'throw', 'try', 'while', 'yield',
+                ],
+            ],
+            'ordered_class_elements'          => [
+                'order' => [
+                    'use_trait', 'constant_public', 'constant_protected', 'constant_private',
+                    'property_public_static', 'property_protected_static', 'property_private_static',
+                    'property_public', 'property_protected', 'property_private', 'method_public_static',
+                    'construct', 'destruct', 'magic', 'phpunit', 'method_public', 'method_protected',
+                    'method_private', 'method_protected_static', 'method_private_static',
                 ],
             ],
         ];
