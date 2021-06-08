741edc60c32c889ea5a648fb4863d5140a876d2d
Kei <budi03@gmail.com>
2021-06-08 22:38:45 +0700

    C

--- src/RuleSet/Drupal.php
+++ src/RuleSet/Drupal.php
@@ -11,16 +11,18 @@ final class Drupal extends AbstractRuleSet implements RuleSetExplicitInterface
      * - https://github.com/drupol/phpcsfixer-configs-drupal/blob/master/src/Config/Drupal8.php
      * - https://github.com/drupol/phpcsfixer-configs-drupal/blob/master/config/drupal/phpcsfixer.rules.yml
      * - https://github.com/drupol/phpcsfixer-configs-drupal/blob/master/config/drupal8/phpcsfixer.rules.yml
+     *
+     * Diff
+     * - [D] yoda_style.always_move_variable
+     * - [D] brace.position_after_anonymous_constructs
      */
     public function myRules(): array
     {
         $rules = [
-            'Drupal/blank_line_before_end_of_class' => true,
-            'Drupal/control_structure_braces_else'  => true,
-            'Drupal/inline_comment_spacer'          => true,
-            'Drupal/try_catch_block'                => true,
-
-            '@PSR2'                                 => false,
+            // 'Drupal/blank_line_before_end_of_class' => true,
+            // 'Drupal/control_structure_braces_else'  => true,
+            // 'Drupal/inline_comment_spacer'          => true,
+            // 'Drupal/try_catch_block'                => true,
             'array_syntax'                          => true,
             'blank_line_before_statement'           => [
                 'statements' => ['break', 'case', 'continue', 'declare', 'default', 'do', 'exit', 'for', 'foreach', 'goto', 'if', 'include', 'include_once', 'require', 'require_once', 'return', 'switch', 'throw', 'try', 'while', 'yield', 'yield_from'],
@@ -29,22 +31,13 @@ final class Drupal extends AbstractRuleSet implements RuleSetExplicitInterface
                 'allow_single_line_closure'                   => true,
                 'position_after_functions_and_oop_constructs' => 'same',
                 'position_after_control_structures'           => 'same',
-                'position_after_anonymous_constructs'         => 'same',
             ],
-            'comment_to_phpdoc'         => false,
             'compact_nullable_typehint' => true,
-            'constant_case'             => [
-                'case' => 'upper',
-            ],
-            'declare_equal_normalize' => [
-                'space' => 'single',
-            ],
-
-            'encoding'                              => true,
-            'full_opening_tag'                      => true,
-            'function_declaration'                  => [
-                'closure_function_spacing' => 'one',
-            ],
+            'constant_case'             => ['case' => 'upper'],
+            'declare_equal_normalize'   => ['space' => 'single'],
+            'encoding'                  => true,
+            'full_opening_tag'          => true,
+            'function_declaration'      => true,
             'general_phpdoc_annotation_remove' => [
                 'annotations' => [
                     'package',
@@ -58,13 +51,8 @@ final class Drupal extends AbstractRuleSet implements RuleSetExplicitInterface
                 'import_constants' => false,
                 'import_functions' => false,
             ],
-            'indentation_type'                   => false,
             'line_ending'                        => true,
             'lowercase_keywords'                 => true,
-            'class_attributes_separation'        => false,
-            'no_alternative_syntax'              => false,
-            'no_blank_lines_after_class_opening' => false,
-            'no_blank_lines_after_phpdoc'        => false,
             'no_extra_blank_lines'               => [
                 'tokens' => ['break', 'case', 'continue', 'default', 'extra', 'parenthesis_brace_block', 'return', 'square_brace_block', 'switch', 'throw', 'use', 'use_trait'],
             ],
@@ -78,7 +66,6 @@ final class Drupal extends AbstractRuleSet implements RuleSetExplicitInterface
                 'allow_unused_params' => true,
                 'remove_inheritdoc'   => false,
             ],
-            'phpdoc_add_missing_param_annotation' => false,
             'single_line_comment_style'           => true,
             'single_quote'                        => [
                 'strings_containing_single_quote_chars' => false,
@@ -90,7 +77,6 @@ final class Drupal extends AbstractRuleSet implements RuleSetExplicitInterface
                 'equal'                => false,
                 'identical'            => false,
                 'less_and_greater'     => false,
-                'always_move_variable' => false,
             ],
 
         ];
