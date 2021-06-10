2c0e0482a6372c59958777ddf9ecfdf1a7225cb9
Kei <budi03@gmail.com>
2021-06-11 04:36:31 +0700

    Update AbstractRuleSet.php

--- src/RuleSet/AbstractRuleSet.php
+++ src/RuleSet/AbstractRuleSet.php
@@ -4,37 +4,14 @@ namespace Realodix\PhpCsFixerConfig\Ruleset;
 
 abstract class AbstractRuleset implements RulesetInterface
 {
-    /**
-     * Name of the ruleset.
-     *
-     * @var string
-     */
     protected $name;
 
-    /**
-     * Rules for the ruleset.
-     *
-     * @var array
-     */
     protected $rules = [];
 
-    /**
-     * Minimum PHP version.
-     *
-     * @var int
-     */
     protected $requiredPHPVersion = 0;
 
-    /**
-     * Have this ruleset turn on `$isRiskyAllowed` flag?
-     *
-     * @var bool
-     */
     protected $autoActivateIsRiskyAllowed = false;
 
-    /**
-     * {@inheritDoc}
-     */
     final public function getName(): string
     {
         $class = strrchr(self::class, '\\') ?: self::class;
@@ -42,25 +19,16 @@ abstract class AbstractRuleset implements RulesetInterface
         return $this->name ?? trim($class, '\\');
     }
 
-    /**
-     * {@inheritDoc}
-     */
     final public function getRules(): array
     {
         return $this->rules;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     final public function getRequiredPHPVersion(): int
     {
         return $this->requiredPHPVersion;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     final public function willAutoActivateIsRiskyAllowed(): bool
     {
         return $this->autoActivateIsRiskyAllowed;
