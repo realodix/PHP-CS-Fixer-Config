cafc194df405cc07aa0fa939d1a89881eb080e5b
Kei <budi03@gmail.com>
2021-06-12 11:53:09 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,9 +4,7 @@ use Realodix\PhpCsFixerConfig\Factory;
 use Realodix\PhpCsFixerConfig\RuleSet;
 
 $overrideRules = [
-    'Symplify/blank_line_after_strict_types' => true,
-    'Symplify/param_return_and_var_tag_malforms' => true,
-    'Symplify/remove_useless_default_comment' => true,
+    // ..
 ];
 
 $options = [
