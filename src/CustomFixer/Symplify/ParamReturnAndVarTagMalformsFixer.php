df1fefad8b46b61d81d73a56b5d93ec205d94dd0
Kei <budi03@gmail.com>
2021-06-15 10:35:28 +0700

    rename

--- src/CustomFixer/Symplify/ParamReturnAndVarTagMalformsFixer.php
+++ src/CustomFixer/Symplify/ParamReturnAndVarTagMalformsFixer.php
@@ -13,7 +13,7 @@ use SplFileInfo;
  * - https://github.com/symplify/coding-standard/blob/main/src/Fixer/Commenting/ParamReturnAndVarTagMalformsFixer.php
  * - https://github.com/symplify/coding-standard/blob/main/docs/rules_overview.md#paramreturnandvartagmalformsfixer
  */
-final class ParamReturnAndVarTagMalforms extends AbstractSymplifyFixer
+final class ParamReturnAndVarTagMalformsFixer extends AbstractSymplifyFixer
 {
     /**
      * @var string
--- src/RuleSet/RealodixStrict.php
+++ src/RuleSet/RealodixStrict.php
@@ -13,8 +13,9 @@ final class RealodixStrict extends AbstractRuleSet
         $basicRules = (new Realodix())->getRules();
 
         $rules = [
-            'align_multiline_comment'          => ['comment_type' => 'all_multiline'],
-            'general_phpdoc_annotation_remove' => [
+            'Symplify/blank_line_after_strict_types' => true,
+            'align_multiline_comment'                => ['comment_type' => 'all_multiline'],
+            'general_phpdoc_annotation_remove'       => [
                 'annotations' => [
                     // https://github.com/doctrine/coding-standard/blob/cfda1d6/lib/Doctrine/ruleset.xml#L192
                     'api', 'author', 'category', 'copyright', 'created', 'license', 'package', 'since',
